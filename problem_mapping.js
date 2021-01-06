e14133e9d4d762e676d7af2a9c2a49ef48890627
Ricardo Hui <rcdhui@gmail.com>
2021-01-06 16:07:55 +0800

    implement integer increment in an array

--- problem_mapping.js
+++ problem_mapping.js
@@ -65,7 +65,7 @@ problem_mapping = {
                 "passed": 0,
                 "total": 10000
             },
-            "Python: closest_int_same_weight.py": { 
+            "Python: closest_int_same_weight.py": {
                 "passed": 0,
                 "total": 10000
             }
@@ -208,7 +208,7 @@ problem_mapping = {
                 "total": 505
             },
             "Python: int_as_array_increment.py": {
-                "passed": 0,
+                "passed": 505,
                 "total": 505
             }
         },
